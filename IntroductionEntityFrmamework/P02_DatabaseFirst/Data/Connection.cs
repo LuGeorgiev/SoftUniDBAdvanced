a067089bd48211e21e2d580729a737926cfccda5
LuGeorgiev <lubomir_78@abv.bg>
2018-06-06 18:46:14 +0300

    Problem 3 solved

--- IntroductionEntityFrmamework/P02_DatabaseFirst/Data/Connection.cs
+++ IntroductionEntityFrmamework/P02_DatabaseFirst/Data/Connection.cs
@@ -6,6 +6,10 @@ namespace P02_DatabaseFirst.Data
 {
     public static class Connection
     {
+        //work
         public const string ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=SoftUni;Integrated Security=True";
+
+        //Home
+        //public const string ConnectionString = "Server=LUBO-NB\\SQLEXPRESS;Database=WMS;Integrated Security=True";
     }
 }
--- IntroductionEntityFrmamework/P02_DatabaseFirst/StartUp.cs
+++ IntroductionEntityFrmamework/P02_DatabaseFirst/StartUp.cs
@@ -1,7 +1,10 @@
-﻿using P02_DatabaseFirst.Data;
+﻿using P02_DatabaseFirst.Contracts;
+using P02_DatabaseFirst.Data;
 using P02_DatabaseFirst.Data.Models;
+using P02_DatabaseFirst.Utilities;
 using System;
 using System.Linq;
+using System.Text;
 
 namespace P02_DatabaseFirst
 {
@@ -9,19 +12,34 @@ namespace P02_DatabaseFirst
     {
         static void Main()
         {
-            var db = new SoftUniContext();
+            IWriter writer = new ConsoleWriter();
+            
+            using (var db = new SoftUniContext())
+            {
 
+                string employees = PrintAllEmployee(db);
+                writer.WiteLine(employees);
+            }   
+                     
+           
+        }
+
+        private static string PrintAllEmployee(SoftUniContext db)
+        {
             var employees = db
                 .Employees
-                .OrderBy(e=>e.EmployeeId)
+                .OrderBy(e => e.EmployeeId)
                 .ToList();
+
+            var sb = new StringBuilder();
 
             foreach (var employee in employees)
             {
-                Console.WriteLine($"{employee.FirstName} {employee.LastName} " +
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} " +
                     $"{employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
             }
-            
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
