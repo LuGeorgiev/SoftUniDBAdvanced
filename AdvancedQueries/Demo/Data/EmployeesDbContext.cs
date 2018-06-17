4bdb97f63b0f84af4f557fb75f851884d8d8b940
LuGeorgiev <lubomir_78@abv.bg>
2018-06-17 18:03:40 +0300

    Advanced query demo

---
+++ AdvancedQueries/Demo/Data/EmployeesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Demo.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Data
+{
+    public class EmployeesDbContext:DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=LUBO-NB\SQLEXPRESS; Database=Employees; Integrated Security = true");
+            }
+        }
+    }
+}
---
+++ AdvancedQueries/Demo/Program.cs
@@ -0,0 +1,69 @@
+﻿using Demo.Data;
+using Demo.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using Z.EntityFramework.Plus;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new EmployeesDbContext();
+
+            //var employees = new[]
+            //{
+            //    new Employee{FirstName="GIvan", LastName="petrov", Salary=6788.98m },
+            //    new Employee{FirstName="Vpetkan", LastName="Ivanov", Salary=1988.98m },
+            //    new Employee{FirstName="Dragan", LastName="stefanof", Salary=2673.98m },
+            //    new Employee{FirstName="Maria", LastName="Manolova", Salary=3788.98m },
+            //    new Employee{FirstName="Ivanka", LastName="Petrova", Salary=63488.98m },
+            //    new Employee{FirstName="Draganka", LastName="georgieva", Salary=67883.8m },
+            //};
+            //context.Employees.AddRange(employees);
+            //context.SaveChanges();
+
+
+            ////Native SQL Queries - All comulns have to be included, JOINS cannot be used
+            //var query=@"SELECT TOP(2) id,FirstName, LAstName, Salary FROM Employees";
+            //var employees = context
+            //    .Employees
+            //    .FromSql(query)
+            //    .ToArray();
+            //Console.WriteLine(employees.Count());
+
+
+            //// Execute procedures
+            //var query = @"EXEC usp_UpdateSalary {0}, {1}"; //{} params
+
+            //var result = context.Database.ExecuteSqlCommand(query,1,5000);
+            //Console.WriteLine(result);
+
+
+            //Bulk opperations - Z.EntityFramework.Plus.EFCore
+
+            //var result = context.Employees.Where(e => e.Salary < 7000).Delete(); 
+            //This si from Z.EF.Plus Can be achieved with RemoveRange from LINQ but it is slower
+
+            //EF Core update
+            //var employees = context.
+            //    Employees
+            //    .Where(e => e.Salary < 3000);
+            //foreach (var employee in employees)
+            //{
+            //    employee.Salary += 200;
+            //}
+            //Bulk update
+            context.Employees
+                .Where(e => e.Salary < 3000)
+                .Update(e => new Employee() { Salary =e.Salary+ 2000m });
+
+            context.SaveChanges();
+                       
+
+
+        }
+    }
+}
